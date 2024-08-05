7974470409a6abbda76b8c106abd0099934ffa68
KazimDandir <kikbal.dandir@gmail.com>
2024-08-05 11:20:20 +0300

    update API

--- ECommerce.API/Controllers/UserController.cs
+++ ECommerce.API/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using ECommerce.Entities;
 using ECommerce.Services.Abstract;
+using ECommerce.Services.DTO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -34,16 +35,61 @@ namespace ECommerce.API.Controllers
             return Ok(user);
         }
 
+        //[HttpPost, Route("[action]")]
+        //public async Task<IActionResult> CreateUser(ApplicationUser user)
+        //{
+        //    var createdUser = await _userService.CreateUserAsync(user);
+        //    return Ok(createdUser);
+        //}
+
         [HttpPost, Route("[action]")]
-        public async Task<IActionResult> CreateUser(ApplicationUser user)
+        public async Task<IActionResult> CreateUser(ApplicationUserDTO userDTO)
         {
+            // {
+            //   "userName": "kazim.dandir",
+            //   "email": "kazim.dandir@example.com",
+            //   "fullName": "Kazim Ikbal Dandir",
+            //   "role": 0
+            // }
+            var user = new ApplicationUser
+            {
+                UserName = userDTO.UserName,
+                Email = userDTO.Email,
+                FullName = userDTO.FullName,
+                Role = userDTO.Role
+            };
+
             var createdUser = await _userService.CreateUserAsync(user);
             return Ok(createdUser);
         }
 
+        //[HttpPut, Route("[action]")]
+        //public async Task<IActionResult> UpdateUser(ApplicationUser user)
+        //{
+        //    var updatedUser = await _userService.UpdateUserAsync(user);
+        //    return Ok(updatedUser);
+        //}
+
         [HttpPut, Route("[action]")]
-        public async Task<IActionResult> UpdateUser(ApplicationUser user)
+        public async Task<IActionResult> UpdateUser(string userId, ApplicationUserDTO userDTO)
         {
+            // {
+            //   "userName": "kazim.dandir",
+            //   "email": "kazim.dandir@newdomain.com",
+            //   "fullName": "Kazim Ikbal Dandir",
+            //   "role": 1
+            // }
+
+            var user = await _userService.GetUserByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.Email = userDTO.Email;
+            user.FullName = userDTO.FullName;
+            user.Role = userDTO.Role;
+
             var updatedUser = await _userService.UpdateUserAsync(user);
             return Ok(updatedUser);
         }
---
+++ ECommerce.Services/DTO/ApplicationUserDTO.cs
@@ -0,0 +1,17 @@
+﻿using ECommerce.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Services.DTO
+{
+    public class ApplicationUserDTO
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public UserRole Role { get; set; }
+    }
+}
